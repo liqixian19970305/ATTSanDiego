05149fab52e17ba6060c1c8ed78972525ed00634
hack10hunter <hack10hunter@gmail.com>
2016-10-29 18:01:40 -0700

    POI test

--- ATTSanDiego/ViewController.swift
+++ ATTSanDiego/ViewController.swift
@@ -12,7 +12,7 @@ import GoogleMaps
 class ViewController: UIViewController, GMSMapViewDelegate {
     // You don't need to modify the default init(nibName:bundle:) method.
     
-    override func loadView() {
+    override func loadView() {	
         // Create a GMSCameraPosition that tells the map to display the
         // coordinate -33.86,151.20 at zoom level 6.
         let camera = GMSCameraPosition.camera(withLatitude: -33.86, longitude: 151.20, zoom: 6.0)
