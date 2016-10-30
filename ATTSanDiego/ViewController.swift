ac2db200b7520ffac117e0f17c2291352c2493ea
liqixian19970305 <LQX@Tonys-MacBook-Pro-2.local>
2016-10-29 19:10:24 -0700

    aaa

--- ATTSanDiego/ViewController.swift
+++ ATTSanDiego/ViewController.swift
@@ -7,40 +7,13 @@
 //
 
 import UIKit
-import GoogleMaps
 
-class ViewController: UIViewController, GMSMapViewDelegate {
-    // You don't need to modify the default init(nibName:bundle:) method.
-    
-    override func loadView() {      
-        // Create a GMSCameraPosition that tells the map to display the
-        // coordinate -33.86,151.20 at zoom level 6.
-        let camera = GMSCameraPosition.camera(withLatitude: -33.86, longitude: 151.20, zoom: 6.0)
-        let mapView = GMSMapView.map(withFrame: CGRect.zero, camera: camera)
-        mapView.delegate = self
-        mapView.isMyLocationEnabled = true
-        view = mapView
-
-    }
+class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
     
-    let infoMarker = GMSMarker()
-    
-    func mapView(_ mapView: GMSMapView, didTapPOIWithPlaceID placeID: String,
-                 name: String, location: CLLocationCoordinate2D) {
-        infoMarker.snippet = placeID
-        infoMarker.position = location
-        infoMarker.title = name
-        infoMarker.opacity = 0;
-        infoMarker.infoWindowAnchor.y = 1
-        infoMarker.map = mapView
-        mapView.selectedMarker = infoMarker
-        print("You tapped \(name): \(placeID), \(location.latitude)/\(location.longitude)")
-    }
-
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
--- Podfile
+++ Podfile
@@ -1,10 +1,10 @@
-# Uncomment the next line to define a global platform for your project
+# Uncomment this line to define a global platform for your project
 # platform :ios, '9.0'
 
 target 'ATTSanDiego' do
-  # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
+  # Comment this line if you're not using Swift and don't want to use dynamic frameworks
   use_frameworks!
-  pod ‘GoogleMaps’
+  pod 'ArcGIS-Runtime-SDK-iOS'
   # Pods for ATTSanDiego
 
   target 'ATTSanDiegoTests' do
